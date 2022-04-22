114900d9747d74f5129676300b1424e1036440ca
Umer-Aziz <umars25271997@gmail.com>
2022-04-22 11:41:24 +0500

    portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'blue-900': '#0a192f',
+        'dark': '#040c16',
+
+        // gray Color 
+        'gray-250': '#ccd6f6',
+        'gray-550': '#8892b0',
+        'gray-900': '#333333',
+
+      },
+    },
+  },
+  plugins: [],
+}
