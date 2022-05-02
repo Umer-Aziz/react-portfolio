14d057b6f320f65fc8d297bd05b34a6caed5c31c
Umer-Aziz <umars25271997@gmail.com>
2022-05-02 11:41:59 +0500

    final commit

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import App from './App';
 import store from './store';
 import { Provider } from "react-redux";
 
-store.subscribe(() => console.log(store.getState()));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
